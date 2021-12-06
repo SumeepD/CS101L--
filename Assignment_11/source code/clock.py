52bfca9b0f5ad6d14b4ec2060a160e435362224a
SumeepD <90810663+SumeepD@users.noreply.github.com>
2021-12-06 12:50:30 -0600

    Add files via upload

--- Assignment_11/source code/clock.py
+++ Assignment_11/source code/clock.py
@@ -1,18 +1,48 @@
 import time
+
+class Clock(time):
+
+    def __init__():
+
+        while True:
+            seconds = time_second
+            minutes = time_minute
+            hour = time_hour
+            seconds += 1
+            time.sleep(1)
+
+    def tick(second, minutes, hour):
+
+        if(seconds == 60):
+            minutes += 1
+            seconds -= 60
+
+        if(minutes == 60):
+            hour += 1
+            minutes -= 60
+
+        if(hour == 13):
+            hour -= 12
+
+        print(hour, ':',minutes, ':', seconds, am_pm)
+
+    def timer(time_hour, time_minute, time_second, am_pm):
+        time_hour = int(input("What is the current hour?"))
+        time_minute = int(input("What is the current minute?"))
+        time_second = int(input("What is the current second?"))
+        am_pm = input("Is it AM or PM? ")
+
+
+        def get_tick():
+            while(seconds % 2 == 0):
+                print(tick)
+
+
+
 time_hour = int(input("What is the current hour?"))
 time_minute = int(input("What is the current minute?"))
 time_second = int(input("What is the current second?"))
-am_pm = input("Is it AM or PM?")
-seconds = time_second
-minutes = time_minute
-hour = time_hour
-while True:
-    seconds += 1
-    time.sleep(1)
-    if(seconds == 60):
-        minutes += 1
-        seconds -= 60
-    print(hour, ':',minutes, ':', seconds, am_pm)
+am_pm = input("Is it AM or PM? ")
 
 
 
